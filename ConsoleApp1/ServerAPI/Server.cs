86bfcc06ba81b38dcafbfa6a4dd9aa6c3f3ed093
Alex <alexrezon@mail.ru>
2024-07-02 20:17:40 +0300

    1
    1

---
+++ ConsoleApp1/ServerAPI/Server.cs
@@ -0,0 +1,20 @@
+﻿using NetFrameworkClasses;
+
+namespace ServerAPI
+{
+    public class Server
+    {
+        public static string url = "https://localhost:7184";
+        public List<Info> infos = new List<Info>();
+        public void ConfigureServices(IServiceCollection services){}
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseRouting();
+        }
+    }
+}
